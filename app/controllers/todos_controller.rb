1f786ecb6fccf5f53c5c0852053e48cecb1d6512
Michael Roxas <work.michaelroxas@gmail.com>
2021-10-18 21:11:16 +0800

    Add destroy function

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,9 +4,16 @@ class TodosController < ApplicationController
         @todo = @category.todos.create(todo_params)
         redirect_to category_path(@category)
       end
+
+    def destroy
+      @category = Category.find(params[:category_id])
+      @todo = @category.todos.find(params[:id])
+      @todo.destroy
+      redirect_to category_path(@category)
+    end
     
       private
         def todo_params
-          params.require(:todo).permit(:todo, :description, :due_date)
+          params.require(:todo).permit(:todo, :due_date)
         end
 end
